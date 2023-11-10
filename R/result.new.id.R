0bff8563efd8536653859b5217ac876245272930
Stefan Palan <stefan.palan@uni-graz.at>
2023-11-10 23:42:08 +0100

    result.save now records date and time of update as well as R and package version information.

--- R/result.new.id.R
+++ R/result.new.id.R
@@ -1,37 +1,54 @@
 #' result.new.id
 #'
 #' Draws a new, unique, random result ID.
-#' @param n number of random ID characters
-#' @param resultvar result variable to check for ID uniqueness
+#' @param id.n The number of random ID characters.
+#' @param resultvar a character vector containing the name of the result
+#' variable to check for ID uniqueness
+#' @param use_lowercase a logical value indicating whether lower case letters 
+#' should be used for the ID
+#' @param use_uppercase a logical value indicating whether upper case letters 
+#' should be used for the ID
+#' @param use_numeric a logical value indicating whether numeric digits' should
+#' be used for the ID
 #' @keywords results, unique ID
 #' @export
 #' @examples
+#' # Create a new, random, 4-character ID that is unique among all IDs in the
+#' 'Result' results object:
+#' 
 #' result.new.id()
-#' result.new.id(n = 4, resultvar = "Result")
-#' Creates a new, random, 4-character ID that is unique among all IDs in the 'Result' results object
+#' result.new.id(id.n = 4, resultvar = "Result")
+#' 
+#' 
+#' # Creates a new, random, 6-character ID that uses lower case and upper case
+#' letter but no digits and is unique among all IDs in the 'main' results
+#' object:
+#' 
 #' result.new.id(6, resultvar = "main", use_uppercase = T, use_numeric = F)
-#' Creates a new, random, 6-character ID that is unique among all IDs in the 'main' results object
 
-result.new.id<-function(n=4,resultvar,use_lowercase=T,use_uppercase=F,use_numeric=T){
+
+result.new.id <- function(id.n = 4, resultvar = "Result", use_lowercase = T, use_uppercase = F, use_numeric = T){
   
   # Checks ------------------------------------------------------------------
   
   # if(use_lowercase | use_uppercase | use_numeric == F) stop("you need to allow at least one type of symbol (lowercase, uppercase, numeric)")
   
   # Defines vectors of legal symbols to use to avoid symbols that may look alike in some fonts
-  legal.lowercase<-c("a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z")
-  legal.uppercase<-c("A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z")
-  legal.numeric<-2:9
-  legal.all<-c(if(use_lowercase){legal.lowercase}else{NULL},if(use_uppercase){legal.uppercase}else{NULL},if(use_numeric){legal.numeric}else{NULL})
+  legal.lowercase <- c("a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z")
+  legal.uppercase <- c("A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z")
+  legal.numeric <- 2:9
+  legal.all <- c(if (use_lowercase) {legal.lowercase} else {NULL},
+                 if (use_uppercase) {legal.uppercase} else {NULL},
+                 if (use_numeric) {legal.numeric} else {NULL})
   
-  # Draws new random id of length n
+  # Draws new random id of length id.n
   repeat{
     
     # Draws random id
-    id<-paste0(sample(legal.lowercase,1),paste(sample(legal.all,size=n-1,replace=T),collapse="", sep=""))
+    id <- paste0(sample(legal.lowercase, 1), paste(sample(legal.all, size = id.n - 1, replace = T), collapse = "", sep = ""))
     
     # Leaves loop only if id is unique
-    if(!id%in%names(resultvar)) break
+    if (!id %in% names(resultvar)) break
   }
   
   return(id)
--- R/result.save.R
+++ R/result.save.R
@@ -7,105 +7,96 @@
 #' @param label designation of this result in the research article (e.g., 'Table 2')
 #' @param type type of result, i.e., 'Regression', 'Pairwise t-Test', etc.
 #' @param description longer description of the result, e.g., 'Test for a treatment effect in the pooled data.'
+#' @param id.n the number of random ID characters.
+#' @param use_lowercase a logical value indicating whether lower case letters 
+#' should be used for the ID
+#' @param use_uppercase a logical value indicating whether upper case letters 
+#' should be used for the ID
+#' @param use_numeric a logical value indicating whether numeric digits' should
+#' be used for the ID
 #' @keywords results, documentation
 #' @export
 #' @examples
-#' Instead of writing, for example:
-#' A[A$a==B$a & A$b==B$b & A$c==B$c & A$d==B$d,]
-#' one can write
-#' A[SPSubset(A,B,c("a","b","c","d")),]
+#' result.save(table(x),
+#'             label = "Table 1",
+#'             type = "Summary table",
+#'             description = "Table listing the experimental conditions.")
 
-# Testdata
-data <- c(1,2,3,4,4,5)
-id <- "ashg"
-label <- "Table 2"
-type <- "Regression"
-description <- "A table consisting of three regression models exploring..."
 
 #######################################
 # Add dump of current package versions (for each result)!
 #######################################
 
-result.save <- function(data=NULL,
-                        id=NA,
-                        resultvar="Result",
-                        label=NA,
-                        type=NA,
-                        description=NA,
-                        id.n=4
+result.save <- function(data = NULL,
+                        id = NA,
+                        resultvar = "Result",
+                        label = NA,
+                        type = NA,
+                        description = NA,
+                        id.n = 4,
+                        use_lowercase = T,
+                        use_uppercase = F,
+                        use_numeric = T
                         ) {
 
-
+  
+  
   # Checks ------------------------------------------------------------------
 
-  # if(!is.numeric(precision)) stop("precision must be numeric")
-  # if(!is.numeric(conf.levels)) stop("conf.levels must be numeric")
-  # if(!is.numeric(ylim)) stop("ylim must be numeric")
-  # if (precision<=0 & precision <1) stop("precision must be positive and less than 1") # Checks precision is in allowed range
+  if (is.null(data)) stop("you need to provide data to save")
 
 
 
   # Packages ----------------------------------------------------------------
-
-  # require(ggplot2)
-  # require(tidyverse)
-
-
-
-
-  # Functions ---------------------------------------------------------------
-
-  result.new.id<-function(resultvar,n=4){
-
-    # Defines vector of legal symbols to use
-    legal.lowercase<-c("a","b","c","d","e","f","g","h","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z")
-    # legal.uppercase<-c("A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z")
-    legal.numeric<-2:9
-
-    # Draws new random id of length n
-    repeat{
-
-      # Draws random id
-      id<-paste0(sample(legal.lowercase,1),paste(sample(c(legal.lowercase,legal.numeric),size=n-1,replace=T),collapse="", sep=""))
-
-      # Leaves loop only if id is unique
-      if(!id%in%names(resultvar)) break
-    }
-
-    return(id)
-  }
-
-
-
+  
+  require(SPResults)
+  
+  
+  
   # Code --------------------------------------------------------------------
-
-  id.set=!is.na(id)
-
-  if(exists(resultvar,envir=.GlobalEnv)){
-    r.orig<-get(resultvar, envir=.GlobalEnv)
+  
+  # Check if user provided an ID
+  id.set <- !is.na(id)
+  
+  # Check if results object exists, otherwise create it
+  if (exists(resultvar, envir = .GlobalEnv)) {
+    r.orig <- get(resultvar, envir = .GlobalEnv)
   } else {
-    r.orig<-list()
+    r.orig <- list()
+    r.orig[["Summary"]]<-list()
+    r.orig[["Summary"]][["LastUpdate"]]<-Sys.time()
+    r.orig[["Summary"]][["sessionInfo"]]<-sessionInfo()
   }
-
-  if(is.na(id)) {
-    id<-result.new.id(r.orig,n=id.n)
+  
+  # Generates ID if none was handed over
+  if (is.na(id)) {
+    id <- result.new.id(id.n = id.n, resultvar = r.orig, use_lowercase = use_lowercase, use_uppercase = use_uppercase, use_numeric = use_numeric)
   }
+  
+  # Prepares temporary object for current result
+  r <- list()
+  r$id <- id
+  r$label <- label
+  r$type <- type
+  r$description <- description
+  r$data <- data
+  
+  # Saves result to results object
+  r.orig[[id]] <- r
+  r.orig[["Summary"]][["LastUpdate"]]<-Sys.time()
+  
+  # Checks package version information, warns in case of changes and updates
+  if(!identical(r.orig[["Summary"]][["sessionInfo"]],sessionInfo())){
+    warning("Detected change in loaded packages since the last result was added.")
+    r.orig[["Summary"]][["sessionInfo"]]<-sessionInfo()
+  }
+  
+  # Assigns updated results object back to global environment
+  assign(resultvar, r.orig, envir = .GlobalEnv)
 
-  r<-list()
-  r$id<-id
-  r$label<-label
-  r$type<-type
-  r$description<-description
-  r$data<-data
-
-  r.orig[[id]]<-r
-
-  assign(resultvar,r.orig,envir=.GlobalEnv)
-
-  # Prints id if id was not set by user
-  if(!id.set) {
-    print(paste("The ID is:"))#,id))
+  # Prints ID if ID was not set by user
+  if (!id.set) {
+    print(paste("The ID is:")) # ,id))
     return(id)
-
   }
 }
